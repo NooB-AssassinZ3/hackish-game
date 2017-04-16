481e9f72fe41fafd4cb18d117a25097821e52fb3
NooB-AssassinZ3 <noob.assassinz3orstep@gmail.com>
2017-04-16 15:44:56 -0500

    work

--- config.sh
+++ config.sh
@@ -3,6 +3,10 @@
 usage() {
 	echo "hacki(sh) game config usage:"
 	echo "-h|--help: Displays Help"
+	echo ""
+	echo "Commands:"
+	echo "set <key> <data>: Sets the value of key <key> to <data>"
+	echo "listkeys: lists all keys"
 }
 if ["$1" = ""];then
 	usage
